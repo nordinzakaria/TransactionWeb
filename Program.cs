ebfceff4636c3be6addcbe81dd7c843bde804e73
nordinzakaria <nordinzakaria@gmail.com>
2024-10-18 17:14:43 +0800

    a bit more

--- Program.cs
+++ Program.cs
@@ -6,9 +6,10 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 // Add this
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7173/") });
 builder.Services.AddSingleton<TransactionPackage.Business>();
 
 
